b8f669d65441854dfe97b40d8f686f61cd260648
msmabel <mabel.bapela@gmail.com>
2023-12-23 16:21:32 +0200

    removed loaded

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1></h1>
         <Weatherengine />
       </header>
     </div>
--- src/Weather-engine.js
+++ src/Weather-engine.js
@@ -1,59 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
-export default function Weatherengine() {
+export default function WeatherDataengine() {
+  const [searchData, setsearchData] = useState({ loaded: false });
+
+  function Weatherengine(response) {
+    setsearchData({
+      setLoaded: true,
+      temperture: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      icon: response.data.weather[0].icon,
+      description: response.data.weather[0].description,
+    });
+  }
+}
+function changeCity(event) {
+  setCity(event.target.value);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  search();
+}
+
+function search() {
+  let city = London;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3a94f3778290bfeee61278505dbbe51d&units=metric`;
+  axios.get(url).then(Weatherengine);
+}
+
+if (searchData.ready) {
   return (
-    <div className="container">
-      <div className="weather-app">
-        <form className="search-form" id="search-form">
+    <div>
+      <div className="container">
+        <div className="weather-app">
+          <form className="search-form" onSubmit={handleSubmit}>
+            <div className="row">
+              <div className="col-6">
+                <input
+                  type="search"
+                  placeholder="Enter any city.."
+                  autofocus="on"
+                  autocomplete="off"
+                  onChange={changeCity}
+                  class="form-control shadow-sm"
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="form-control btn btn-primary shadow-sm w-100"
+                />
+              </div>
+              <div className="col-3">
+                <button
+                  type="button"
+                  className="btn btn-light"
+                  id="current-location-button"
+                >
+                  Current
+                </button>
+              </div>
+            </div>
+          </form>
+          <h1> {searchData.city}</h1>
+          <ul>
+            <li id="date">Wednesday 12:55</li>
+            <li id="description"> {searchData.description}</li>
+          </ul>
           <div className="row">
-            <div className="col-6">
-              <input
-                type="search"
-                placeholder="Type any city.."
-                autofocus="on"
-                autocomplete="off"
-                id="city-input"
-                class="form-control shadow-sm"
+            <div className="clearfix">
+              <img
+                src="http://openweathermap.org/img/wn/01n@2x.png"
+                alt="Clear"
+                id="icon"
+                class="float-left"
               />
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="Search"
-                className="form-control btn btn-primary shadow-sm w-100"
-              />
-            </div>
-            <div className="col-3">
-              <button
-                type="button"
-                className="btn btn-light"
-                id="current-location-button"
-              >
-                Current
-              </button>
-            </div>
-          </div>
-        </form>
-        <h1 id="city">New York</h1>
-        <ul>
-          <li id="date">Wednesday 12:55</li>
-          <li id="description">Clear</li>
-        </ul>
-        <div className="row">
-          <div className="clearfix">
-            <img
-              src="http://openweathermap.org/img/wn/01n@2x.png"
-              alt="Clear"
-              id="icon"
-              class="float-left"
-            />
-            <div className="float-left">
-              <span className="temperature">3°C</span>
+              <div className="float-left">
+                <span className="temperature">{searchData.temperature}</span>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
   );
+} else {
+  search();
+  return "Loading Data..";
 }
