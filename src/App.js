f687230e2646ae050a6b43a44dc8263735998e6c
msmabel <mabel.bapela@gmail.com>
2024-02-07 15:14:04 +0200

    added a container

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import Weatherengine from "./Weather-engine";
 
 function App() {
   return (
-    <div>
-      <header className="App-header">
-        <Weatherengine defaultCity="paris" />
-      </header>
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <Weatherengine defaultCity="paris" />
+        </header>
+
+        <footer>
+          This project was coded by <a href="" target=""></a> Mabel Bapela
+        </footer>
+      </div>
     </div>
   );
 }
